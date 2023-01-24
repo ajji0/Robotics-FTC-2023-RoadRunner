f44821943bf1912a713579c8aad3d6936f8aeaec
ajji0 <ajdabbeast@gmail.com>
2023-01-23 20:35:44 -0500

    Finished project

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/opmode/teleop/xDrive.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/opmode/teleop/xDrive.java
@@ -2,6 +2,7 @@ package org.firstinspires.ftc.teamcode.drive.opmode.teleop;
 
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.util.ElapsedTime;
 
 //Declaring Teleop mode and name of mode
@@ -10,6 +11,15 @@ public class xDrive extends LinearOpMode {
     xDriveHardware robot = new xDriveHardware();
     private ElapsedTime runtime = new ElapsedTime();
 
+    // variable to represent the arm stage 0-3
+    public int stage = 0;
+
+
+    //Stage Each Stage Length Increase or decrease these values depending on your height needed
+    public int stage0Length = 0;
+    public int stage1Length = 20;
+    public int stage2Length = 30;
+    public int stage3Legnth = 40;
 
     @Override
     public void runOpMode(){
@@ -17,7 +27,8 @@ public class xDrive extends LinearOpMode {
         // Init the robot hardware map(Motors servos)
         robot.init(hardwareMap);
 
-
+        // Saves the position of when linear slide is on the ground
+        int stage0 = robot.armMotor.getCurrentPosition();
 
         // Update Driver Hub or Phone that the robot is ready to run
         telemetry.addData("Status:", "Ready to Run");
@@ -26,7 +37,7 @@ public class xDrive extends LinearOpMode {
         waitForStart();
         runtime.reset();
 
-        // Shows starting postition of the arm
+        // Shows starting position of the arm
         telemetry.addData("Arm_Starting_at...:", "%7d", robot.armMotor.getCurrentPosition());
         telemetry.update();
 
@@ -59,7 +70,7 @@ public class xDrive extends LinearOpMode {
             }
 
             // Uses the Method to set all motor powers then Ten Telemetry to see how much power each is getting
-            robot.setDriveMotorPower(rightFrontPower, leftBackPower, rightBackPower); // removed leftFront Because did not have expansion hub on at the time and needed to test
+            robot.setDriveMotorPower(leftFrontPower, rightFrontPower, leftBackPower, rightBackPower); // removed leftFront Because did not have expansion hub on at the time and needed to test
             telemetry.addData("Status", "Run Time: " + runtime.toString());
             telemetry.addData("Front left/Right", "%4.2f", rightFrontPower);
             telemetry.addData("Back  left/Right", "%4.2f, %4.2f", leftBackPower, rightBackPower);
@@ -67,14 +78,83 @@ public class xDrive extends LinearOpMode {
             // Uses the ArmStage Method to increase the stage if X is pressed and decrease if a is pressed
             telemetry.addData("Arm Motor Locat:", "%7d", robot.armMotor.getCurrentPosition());
 
-            robot.ArmStageIncrease(gamepad1.x);
-            robot.ArmStageDecrease(gamepad1.a);
-            robot.ArmSetStage0(gamepad1.b);
+            //First make sures the motor is not busy
+            if (!robot.armMotor.isBusy()){
+                // Checks if the user has pressed gamepad button
+                if (gamepad1.x && stage < 3) {
 
+                    //Add increase the stage
+                    stage++;
 
+                    //Checks for each stage and sets the robots height to that stage
+                    if (stage == 1) {
+                        robot.ArmToPosition(robot.TURN_SPEED, stage1Length);
+                    }
 
-            telemetry.addData("Stage", "%7d", robot.stage);
+                    else if (stage == 2) {
+                        robot.ArmToPosition(robot.TURN_SPEED, stage2Length);
+                    }
+
+                    else if (stage == 3) {
+                        robot.ArmToPosition(robot.TURN_SPEED, stage3Legnth);
+                    }
+
+                }
+            }
+
+            //Same thing as the increase but decreases
+            if (!robot.armMotor.isBusy()) {
+                if (gamepad1.a && stage >= 0) {
+                    stage--;
+                    // Sets Turn Speed to Negative so motor rewinds
+                    if (stage == 0){
+                        robot.armMotor.setTargetPosition(stage0);
+                        robot.armMotor.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+                    }
+
+                    if (stage == 1) {
+                        robot.ArmToPosition(robot.TURN_SPEED, -stage1Length);
+                    }
+
+                    else if (stage == 2) {
+                        robot.ArmToPosition(robot.TURN_SPEED, -stage2Length);
+                    }
+
+                    else if (stage == 3) {
+                        robot.ArmToPosition(robot.TURN_SPEED, -stage3Legnth);
+                    }
+
+                }
+            }
+            //Sets Stage to 0 and sets the robot position to the original saved position
+            if (gamepad1.b) {
+                stage = 0;
+                robot.ArmToPosition(robot.TURN_SPEED, stage0Length);
+            }
+
+            //ArmStageIncrease();  Note: Was commented out
+            //ArmStageDecrease();
+
+            telemetry.addData("Stage", "%7d", stage);
             telemetry.update();
-        }
+
+
+            // Commented this out for now
+            // Method to increase the Arm for each stage check using a button
+        /*public void ArmStageIncrease() {
+            // Checks if the button then adds one to the stage
+            // checks what stage the stage is on then multiples the length but that amount
+        }*/
+
+            // Same method But decreases the Stage
+        /*public void ArmStageDecrease(){
+            // Make sure the Stage is not zero and rewinding
+        }*/
+
+
+        /*public void ArmSetStage0(){
+            stage0 = robot.armMotor.getCurrentPosition();
+        */}
+
     }
 }
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/opmode/teleop/xDriveHardware.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/opmode/teleop/xDriveHardware.java
@@ -1,16 +1,14 @@
 package org.firstinspires.ftc.teamcode.drive.opmode.teleop;
 
-
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorSimple;
 import com.qualcomm.robotcore.hardware.HardwareMap;
 import com.qualcomm.robotcore.hardware.Servo;
 
-
 public class xDriveHardware{
 
-    // Declare Drive Motor Variables
-    //public DcMotor leftFront = null; // removed leftFront Because did not have expansion hub on at the time and needed to test
+    // Declare Drive Motor Variaibles
+    public DcMotor leftFront = null; // removed leftFront Because did not have expansion hub on at the time and needed to test
     public DcMotor rightFront = null;
     public DcMotor leftBack = null;
     public DcMotor rightBack = null;
@@ -19,7 +17,9 @@ public class xDriveHardware{
     public DcMotor armMotor = null;
 
     //Place holder variable for when a servo is added to robot
-    public Servo hook = null;
+    public Servo claw = null;
+
+    public int startPosition = 0;
 
     // Converting MotorTicks, Gear Ratio, Spool/ Wheel Diameter to Counts Per Inch for Encoder
     public static final double     COUNTS_PER_MOTOR_REV    = 1440 ;    // eg: TETRIX Motor Encoder
@@ -27,18 +27,16 @@ public class xDriveHardware{
     public static final double     SPOOL_DIAMETER_INCHES   = 4.0 ;     // For figuring circumference
     public static final double     COUNTS_PER_INCH         = (COUNTS_PER_MOTOR_REV * DRIVE_GEAR_REDUCTION) /
             (SPOOL_DIAMETER_INCHES * 3.1415);
+
     // Spools Turn speed
-    public static final double     TURN_SPEED              = 0.7;
+    public static double     TURN_SPEED              = 0.7;
+    
     // Stage Length of linear slide stage in inches
-    public int stageLength = 20;
+
 
     HardwareMap hwMap  = null;
 
-    // variable to represent the arm stage 0-3
-    public int stage = 0;
 
-    // variable to represent stage 0
-    public int stage0;
 
     // Constructor
     public xDriveHardware(){
@@ -52,28 +50,27 @@ public class xDriveHardware{
         // Initialize the hardware variables. Note that the strings used here as parameters
         // to 'get' must correspond to the names assigned during the robot configuration
         // step (using the FTC Robot Controller app on the phone).
-
-        /*leftFront = hwMap.get(DcMotor.class, "leftFront");
-        Note: This Motor was Commented out for test purposes only remove when done
-         */
+        leftFront = hwMap.get(DcMotor.class, "leftFront");
         rightFront = hwMap.get(DcMotor.class, "rightFront");
         leftBack = hwMap.get(DcMotor.class, "leftBack");
         rightBack = hwMap.get(DcMotor.class, "rightBack");
 
         armMotor = hwMap.get(DcMotor.class, "armMotor");
 
+        claw = hwMap.get(Servo.class, "claw");
+
+
         armMotor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
 
         armMotor.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
 
-
+        startPosition = armMotor.getCurrentPosition();
 
 
         // To drive forward, most robots need the motor on one side to be reversed, because the axles point in opposite directions.
         // Pushing the left stick forward MUST make robot go forward. So adjust these two lines based on your first test drive.
         // Note: The settings here assume direct drive on left and right wheels.  Gear Reduction or 90 Deg drives may require direction flips
-
-        //leftFront.setDirection(DcMotor.Direction.REVERSE);
+        leftFront.setDirection(DcMotor.Direction.REVERSE);
         rightFront.setDirection(DcMotor.Direction.FORWARD);
         rightBack.setDirection(DcMotor.Direction.FORWARD);
         leftBack.setDirection(DcMotorSimple.Direction.REVERSE);
@@ -85,8 +82,8 @@ public class xDriveHardware{
 
     }
     /*Method sets each Drive Motors Powers  */
-    public void setDriveMotorPower(double RFPower, double LBPower, double RBPower){// removed leftFront Because did not have expansion hub on at the time and needed to test
-        //leftFront.setPower(LFPower);
+    public void setDriveMotorPower(double LFPower, double RFPower, double LBPower, double RBPower){// removed leftFront Because did not have expansion hub on at the time and needed to test
+        leftFront.setPower(LFPower);
         rightFront.setPower(RFPower);
         leftBack.setPower(LBPower);
         rightBack.setPower(RBPower);
@@ -94,7 +91,7 @@ public class xDriveHardware{
 
     // Sets all the Drive Motor Powers
     public void setAllDrivePower(double allPower){
-        setDriveMotorPower(allPower, allPower, allPower);// removed leftFront Because did not have expansion hub on at the time and needed to test
+        setDriveMotorPower(allPower, allPower, allPower, allPower);// removed leftFront Because did not have expansion hub on at the time and needed to test
     }
 
     //Sets every MotorPower
@@ -103,14 +100,13 @@ public class xDriveHardware{
         armMotor.setPower(allPower);
     }
 
-
     // Moves the arm to the inches passed as parameters
     public void ArmToPosition(double speed, double Inches){
         int newTarget;
 
 
         // Creates Motors new Target Position then sets its target to new position
-        newTarget = armMotor.getCurrentPosition() + (int) (Inches * COUNTS_PER_INCH);
+        newTarget = startPosition + (int) (Inches * COUNTS_PER_INCH);
         armMotor.setTargetPosition(newTarget);
 
         //Sets arm motor to run to target position
@@ -121,52 +117,6 @@ public class xDriveHardware{
 
     }
 
-    // Method to increase the Arm for each stage check using a button
-    public void ArmStageIncrease(boolean button) {
-        // Checks if the button then adds one to the stage
-        // checks what stage the stage is on then multiples the length but that amount
-        if (button) {
-
-            stage++;
-
-            if (stage == 1){
-                ArmToPosition(TURN_SPEED, stage*stageLength);
-            }
-
-            else if (stage == 2){
-                ArmToPosition(TURN_SPEED, stage*stageLength);
-            }
-
-            else if (stage == 3){
-                ArmToPosition(TURN_SPEED, stage*stageLength);
-            }
-
-            //Makes sure the Stage never is greater then 3
-            if (stage > 3){
-                stage = 3;
-            }
-
-        }
-    }
-
-    // Same method But decreases the Stage
-    public void ArmStageDecrease(boolean button){
-        // Make sure the Stage is not zero and rewinding
-        if (button){
-            stage--;
-            // Sets Turn Speed to Negative so motor rewinds
-            ArmToPosition(-TURN_SPEED, -stageLength);
-        }
-    }
 
 
-    public void ArmSetStage0(boolean button){
-        stage0 = armMotor.getCurrentPosition();
-
-        if (button){
-            stage = 0;
-            armMotor.setTargetPosition(stage0);
-            armMotor.setMode(DcMotor.RunMode.RUN_TO_POSITION);
-        }
-    }
 }
